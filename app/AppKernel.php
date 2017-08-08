01162e02a0b4083d8fc8eca611362eaa88dc5b7d
lomboboo <lomboboo@gmail.com>
2017-08-08 14:38:30 +0200

    DoctrineMigrationsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
 	        new FOS\UserBundle\FOSUserBundle(),
+	        new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
